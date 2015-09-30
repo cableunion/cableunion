572ccbf97e3cdb4d2c66f2b02af743a8d86fd7fb
small01 <maxw1991@163.com>
2015-10-01 07:36:28 +0800

    add png

--- Cable_Project/common/static/js/location/area.js
+++ Cable_Project/common/static/js/location/area.js
@@ -5,8 +5,8 @@
             title = ['省份' , '地级市' , '市、县、区'],
             $loc_province = $('#loc_province'),
             $loc_city = $('#loc_city'),
-            $loc_town = $('#loc_town'),
-            $locationId = $('input[name="location_id"]');
+            $loc_town = $('#loc_town');
+//            $locationId = $('input[name="location_id"]');
 
         $.each(title , function(k , v) {
             title[k] = '<option value="">'+v+'</option>';
@@ -22,19 +22,19 @@
             location.fillOption('loc_city' , '0,' + $loc_province.val());
             $loc_town.empty();
             $loc_town.append(title[2]);
-            $locationId.val($(this).val());
+//            $locationId.val($(this).val());
         });
 
         $loc_city.change(function() {
             $loc_town.empty();
             $loc_town.append(title[2]);
             location.fillOption('loc_town', '0,' + $loc_province.val() + ',' + $loc_city.val());
-            $locationId.val($(this).val());
+//            $locationId.val($(this).val());
         });
 
-        $loc_town.change(function() {
-            $locationId.val($(this).val());
-        });
+//        $loc_town.change(function() {
+//            $locationId.val($(this).val());
+//        });
 
         if (province) {
             location.fillOption('loc_province' , '0' , province);
