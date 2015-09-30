f49f965be58f5590312ee3c495d61679d45aaf2f
small01 <maxw1991@163.com>
2015-09-30 15:23:09 +0800

    prod

--- Cable_Project/Cable_Project/settings/prod.py
+++ Cable_Project/Cable_Project/settings/prod.py
@@ -111,6 +111,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,  '../common/static'),
     os.path.join(BASE_DIR,  '../account/static'),
+    os.path.join(BASE_DIR,  '../home/static'),
 )
 
 TEMPLATE_DIRS = (
